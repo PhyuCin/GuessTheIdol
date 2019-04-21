996d810985642d667ef73a88f532deb935b6ca07
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-04-21 23:18:36 +0800

    22:42

--- app/src/main/java/com/example/guesstheidol/SettingsActivityFragment.java
+++ app/src/main/java/com/example/guesstheidol/SettingsActivityFragment.java
@@ -1,5 +1,11 @@
 package com.example.guesstheidol;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,14 +15,21 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class SettingsActivityFragment extends Fragment {
+public class SettingsActivityFragment extends PreferenceFragment
+implements SharedPreferences.OnSharedPreferenceChangeListener{
 
-    public SettingsActivityFragment() {
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        PreferenceManager.getDefaultSharedPreferences(getContext()).registerOnSharedPreferenceChangeListener(this);
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
     }
 }
