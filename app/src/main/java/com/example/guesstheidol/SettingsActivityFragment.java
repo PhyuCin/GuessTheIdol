8ce640bb9875dbf15c966035144281ea9b5d6656
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-04-22 10:09:25 +0800

    handles user's choices from settings activity

--- app/src/main/java/com/example/guesstheidol/SettingsActivityFragment.java
+++ app/src/main/java/com/example/guesstheidol/SettingsActivityFragment.java
@@ -2,15 +2,15 @@ package com.example.guesstheidol;
 
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
-import android.preference.Preference;
+import android.os.Bundle;
+import android.preference.MultiSelectListPreference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Set;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -28,8 +28,37 @@ implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     }
 
+    @SuppressLint("NewApi")
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(getContext()).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @SuppressLint("NewApi")
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.i("SettingsActivityFrag", "on shared preferences changed called" + key);
+        switch(key){
+            case "choicesSelection":
+                break;
+
+            case "groupsSelection":
+                Set<String> groups = sharedPreferences.getStringSet("groupsSelection", null);
+                if(groups == null){
+                    return;
+                }
+                if(groups.size() == 0){
+                    Toast.makeText(getContext(), "You must select at lease one group." +
+                            "\nDefaulting to ASTRO.", Toast.LENGTH_LONG).show();
+                    String defaultGroup = "ASTRO";
+                    groups.add(defaultGroup);
+                    sharedPreferences.edit().putStringSet("groupsSelection", groups).apply();
+                    MultiSelectListPreference pref = (MultiSelectListPreference) findPreference("groupsSelection");
+                    pref.setValues(groups);
+                }
+                break;
+        }
 
     }
 }
